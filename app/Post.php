36c142c4bf48938da41bff24af69e84f1de55461
yuvraj <yuvrajdjadhav@gmail.com>
2018-01-29 02:19:01 -0800

    Signed-off-by: yuvraj <yuvrajdjadhav@gmail.com>

---
+++ app/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    // table name
+    protected $table = 'posts';
+
+    // Primary Key
+    Public $PrimaryKey = 'id';
+    
+    //Timestamps
+    public $timestamps = true;
+
+    public function user(){
+        return $this->belongsTo('App\user');
+    }
+}
